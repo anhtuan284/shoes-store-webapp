f5c1480d47fe61698b8f02b909eb1e9dca399d95
anhtuan284 <peteralwaysloveu@gmail.com>
2024-05-23 14:49:16 +0700

    updateee

---
+++ QLBG.BLL/TagSvc.cs
@@ -0,0 +1,27 @@
+﻿using QLBG.Common.BLL;
+using QLBG.Common.Req;
+using QLBG.Common.Rsp;
+using QLBG.DAL;
+using QLBG.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBG.BLL
+{
+    public class TagSvc : GenericSvc<TagRep,Tag>
+    {
+        TagRep tagRep = new TagRep();
+
+        public SingleRsp CreateTag(TagReq tagReq)
+        {
+            var res = new SingleRsp();
+            Tag tag = new Tag();
+            tag.Name = tagReq.Name;
+            res = tagRep.CreateTag(tag);
+            return res;
+        }
+    }
+}
